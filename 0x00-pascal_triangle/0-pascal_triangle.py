6319de021bdc1cb2854ea3e87ad3d47791614894
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-23 18:26:32 +0000

    new commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+0. Pascal's Triangle
+"""
+
+
 def pascal_triangle(n):
     """Returns a list of lists of integers representing the Pascal’s triangle of n"""
     if n <= 0:
