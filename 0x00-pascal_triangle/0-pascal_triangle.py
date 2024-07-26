ebc709263332d73ceb3e6b4ca982826c4379fdeb
Prince Opoku Amaning <princoprince94@gmail.com>
2024-07-26 23:19:50 +0000

    Added newline

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,7 +5,10 @@
 
 
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing the Pascal’s triangle of n"""
+    """
+    Returns a list of lists of integers representing
+    the Pascal’s triangle of n
+    """
     # if n <= 0:
     #     return []
     # triangle = [[1]]
