aa8f93a79f2b3da9b4ad1c8c148976aa14d4b7e2
Prince Opoku Amaning <princoprince94@gmail.com>
2024-08-18 10:57:40 +0000

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,64 +1,45 @@
 #!/usr/bin/python3
-"""Log parsing"""
+
+"""Script that reads stdin line by line and computes metrics"""
 
 import sys
-import signal
 
 
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+def printsts(dic, size):
+    """ WWPrints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
 
-def print_stats():
-    """Prints the stats"""
-    print("File size: {}".format(total_size))
-    for key, value in sorted(status_codes.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
 
+count = 0
+size = 0
 
-def signal_handler(sig, frame):
-    """Signal handler"""
-    print_stats()
-    sys.exit(0)
-
-
-# Set up signal handler for CTRL+C
-signal.signal(signal.SIGINT, signal_handler)
-
-# Read from stdin
 try:
     for line in sys.stdin:
-        line_count += 1
-        data = line.split()
-        if len(data) < 7:
-            continue
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
 
-        # Get the data
+        stlist = line.split()
+        count += 1
+
         try:
-            ip = data[0]
-            date = data[3][1:]
-            method = data[5][1:]
-            path = data[6]
-            protocol = data[7][:-1]
-            status_code = int(data[8])
-            size = int(data[9])
-        except (ValueError, IndexError):
-            continue
+            size += int(stlist[-1])
+        except:
+            pass
 
-        # Ensure the format matches
-        if (method == 'GET' and path ==
-                '/projects/260' and
-                protocol == 'HTTP/1.1'):
-            total_size += size
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-            line
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
+            pass
+    printsts(sts, size)
 
-        # Print stats every 10 lines
-        if line_count % 10 == 0:
-            print_stats()
+
 except KeyboardInterrupt:
-    print_stats()
-    sys.exit(0)
+    printsts(sts, size)
+    raise
