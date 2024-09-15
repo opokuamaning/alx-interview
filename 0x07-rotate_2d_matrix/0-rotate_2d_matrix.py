4cfdf090102589794021c58f2d26a641684ea849
Prince Opoku Amaning <princoprince94@gmail.com>
2024-09-15 17:28:29 +0000

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
+"""Rotates a 2D matrix"""
 
 
 def rotate_2d_matrix(matrix):
-    # Transpose the matrix
-    for i in range(len(matrix)):
-        for j in range(i, len(matrix)):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    """
+       Rotates 2D matrix 90 degrees clockwise
+       Matrix is edited in-place
+       args:
+          matrix
+    """
+    left, right = 0, len(matrix) - 1
 
-    # Reverse each row
-    for i in range(len(matrix)):
-        matrix[i].reverse()
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            # save topleft  value
+            topLeft = matrix[top][left + i]
+            # move bottom left to top left
+            matrix[top][left + i] = matrix[bottom - i][left]
+            # move bottom right to bottom left
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            # move top right to bottom right
+            matrix[bottom][right - i] = matrix[top + i][right]
+            # move top left to top right
+            matrix[top + i][right] = topLeft
+        right -= 1
+        left += 1
